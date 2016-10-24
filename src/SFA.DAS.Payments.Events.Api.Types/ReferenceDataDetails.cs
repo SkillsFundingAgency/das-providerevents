5ce7cc17024dad0e62c770ee808a07704546862e
Simon Bull <sbulluk@users.noreply.github.com>
2016-10-24 16:55:51 +0100

    Start to shape API return types

---
+++ src/SFA.DAS.Payments.Events.Api.Types/ReferenceDataDetails.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SFA.DAS.Payments.Events.Api.Types
+{
+    public class ReferenceDataDetails
+    {
+        public DateTime AccountDataValidTill { get; set; }
+        public DateTime CommitmentDataValidTill { get; set; }
+    }
+}
---
+++ src/SFA.DAS.Payments.Events.Api/Controllers/PaymentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SFA.DAS.Payments.Events.Api.Controllers
+{
+    [RoutePrefix("api/payments")]
+    public class PaymentController : ApiController
+    {
+        [Route("", Name = "PaymentsList")]
+        public Task<IHttpActionResult> GetListOfPayments(string periodId = null, int page = 1, int pageSize = 1000)
+        {
+            return Task.FromResult<IHttpActionResult>(Ok(new
+            {
+                PeriodId = periodId,
+                Page = page,
+                PageSize = pageSize
+            }));
+        }
+    }
+}
