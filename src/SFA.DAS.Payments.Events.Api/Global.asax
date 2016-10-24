9a9711d816808b2badb807833be9a4968a93ad5c
Simon Bull <sbulluk@users.noreply.github.com>
2016-10-24 15:38:42 +0100

    Scaffold solution

---
+++ src/SFA.DAS.Payments.Events.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SFA.DAS.Payments.Events.Api.WebApiApplication" Language="C#" %>
---
+++ src/SFA.DAS.Payments.Events.Application/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Payments.Events.Application
+{
+    public class Class1
+    {
+    }
+}
