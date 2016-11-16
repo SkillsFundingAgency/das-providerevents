262e4a72a8b1e9d9ac492f7739d673b3cb18b3aa
Simon Bull <sbulluk@users.noreply.github.com>
2016-11-16 09:38:13 +0000

    Wrap errors in client

--- src/SFA.DAS.Payments.Events.Api.Client/SecureHttpClient.cs
+++ src/SFA.DAS.Payments.Events.Api.Client/SecureHttpClient.cs
@@ -1,4 +1,7 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 
 namespace SFA.DAS.Payments.Events.Api.Client
@@ -18,14 +21,40 @@ namespace SFA.DAS.Payments.Events.Api.Client
 
         public virtual async Task<string> GetAsync(string url)
         {
-            using (var client = new HttpClient())
+            try
             {
-                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _clientToken);
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Authorization =
+                        new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _clientToken);
 
-                var response = await client.GetAsync(url);
-                response.EnsureSuccessStatusCode();
+                    var response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
 
-                return await response.Content.ReadAsStringAsync();
+                    return await response.Content.ReadAsStringAsync();
+                }
+            }
+            catch (UriFormatException ex)
+            {
+                throw new BadRequestException("Url is malformed", ex);
+            }
+            catch (HttpRequestException ex)
+            {
+                if (ex.InnerException != null && ex.InnerException is WebException)
+                {
+                    var webEx = (WebException) ex.InnerException;
+                    if (webEx.InnerException != null && webEx.InnerException is SocketException)
+                    {
+                        var sockEx = (SocketException) webEx.InnerException;
+
+                        throw new ServerUnavailableException(ex, sockEx.SocketErrorCode);
+                    }
+                }
+                throw new ApiException("Protocol error with API - " + ex.Message, ex);
+            }
+            catch (Exception ex)
+            {
+                throw new ApiException("Unexpected error in API - " + ex.Message, ex);
             }
         }
     }
