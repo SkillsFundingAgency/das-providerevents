4aa3b7b96f5445ccf2aad09c2be7331f3560fa24
Dan Lewis <Dan.lewis@fasst.org.uk>
2016-11-23 15:39:56 +0000

    Update

--- src/SFA.DAS.Payments.Events.Api/StartUp/IIS.bat
+++ src/SFA.DAS.Payments.Events.Api/StartUp/IIS.bat
@@ -1,4 +1,5 @@
-@echo off 
-powershell -command "Set-ExecutionPolicy Unrestricted" 
-powershell .\IIS.ps1 
-exit /b 0
+REM   Run an unsigned PowerShell script and log the output
+PowerShell -ExecutionPolicy Unrestricted .\IIS.ps1  >> "%TEMP%\StartupLog.txt" 2>&1
+
+REM   If an error occurred, return the errorlevel.
+EXIT /B %errorlevel%
