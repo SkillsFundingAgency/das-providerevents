319e2e917912762abfea833a66630e459896180b
Dan Lewis <Dan.lewis@fasst.org.uk>
2016-11-23 11:22:34 +0000

    Test

--- src/SFA.DAS.Payments.Events.Api/Startup/Startup.bat
+++ src/SFA.DAS.Payments.Events.Api/Startup/Startup.bat
@@ -1,1 +1,4 @@
-powershell -ExecutionPolicy Unrestricted .\IIS.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
+PowerShell.exe -ExecutionPolicy Unrestricted .\IIS.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
+
+REM   If an error occurred, return the errorlevel.
+EXIT /B %errorlevel%
