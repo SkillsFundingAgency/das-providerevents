643201739952afc30ee349cf4febe944bd19eac9
Dan Lewis <Dan.lewis@fasst.org.uk>
2016-11-23 16:36:55 +0000

    update

--- src/SFA.DAS.Payments.Events.Api/StartUp/IIS.bat
+++ src/SFA.DAS.Payments.Events.Api/StartUp/IIS.bat
@@ -1,3 +1,4 @@
 
-PowerShell.exe -ExecutionPolicy Unrestricted ".\IIS.ps1"
+PowerShell.exe -ExecutionPolicy Unrestricted "\IIS.ps1"
+PowerShell.exe -ExecutionPolicy Unrestricted "StartUp\IIS.ps1"
 EXIT /B 0
