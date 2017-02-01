2f9c0b0fe14f983aa8bfbac332a4e89ab1758623
Simon Bull <sbulluk@users.noreply.github.com>
2017-02-01 08:46:31 +0000

    Create DataLock projects

---
+++ src/collections/SFA.DAS.Provider.Events.DataLock/DataLockEventsProcessor.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+
+namespace SFA.DAS.Provider.Events.DataLock
+{
+    public class DataLockEventsProcessor
+    {
+        private readonly IMediator _mediator;
+
+        public DataLockEventsProcessor(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        protected DataLockEventsProcessor()
+        {
+            // For unit testing
+        }
+
+        public virtual void Process()
+        {
+            
+        }
+
+    }
+}
---
+++ src/collections/SFA.DAS.Provider.Events.DataLock/Infrastructure/DependencyResolution/DataLockEventsRegistry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using MediatR;
+using NLog;
+using StructureMap;
+
+namespace SFA.DAS.Provider.Events.DataLock.Infrastructure.DependencyResolution
+{
+    public class DataLockEventsRegistry : Registry
+    {
+        public DataLockEventsRegistry(Type taskType)
+        {
+            Scan(
+                scan =>
+                {
+                    scan.AssemblyContainingType<DataLockEventsRegistry>();
+
+                    scan.RegisterConcreteTypesAgainstTheFirstInterface();
+                });
+
+            For<ILogger>().Use(() => LogManager.GetLogger(taskType.FullName));
+
+            For<SingleInstanceFactory>().Use<SingleInstanceFactory>(ctx => t => GetInstance(ctx, t));
+            For<MultiInstanceFactory>().Use<MultiInstanceFactory>(ctx => t => GetAllInstances(ctx, t));
+            For<IMediator>().Use<Mediator>();
+        }
+
+        private static IEnumerable<object> GetAllInstances(IContext ctx, Type t)
+        {
+            return ctx.GetAllInstances(t);
+        }
+
+        private static object GetInstance(IContext ctx, Type t)
+        {
+            return ctx.GetInstance(t);
+        }
+    }
+}
