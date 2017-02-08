1310b116a0850f0959fe3726cba09a9775a7c924
Simon Bull <simonbu11@users.noreply.github.com>
2017-02-08 08:31:45 +0000

    Add scafolding for data lock events component

---
+++ src/collections/SFA.DAS.Provider.Events.DataLock/DataLockEventsTask.cs
@@ -0,0 +1,34 @@
+﻿using SFA.DAS.Payments.DCFS;
+using SFA.DAS.Payments.DCFS.Context;
+using SFA.DAS.Payments.DCFS.Infrastructure.DependencyResolution;
+using SFA.DAS.Provider.Events.DataLock.Infrastructure.DependencyResolution;
+
+namespace SFA.DAS.Provider.Events.DataLock
+{
+    public class DataLockEventsTask : DcfsTask
+    {
+        private const string SubmissionsSchema = "DataLock";
+
+        private readonly IDependencyResolver _dependencyResolver;
+
+        public DataLockEventsTask() 
+            : base(SubmissionsSchema)
+        {
+            _dependencyResolver = new TaskDependencyResolver();
+        }
+        public DataLockEventsTask(IDependencyResolver dependencyResolver)
+            : base(SubmissionsSchema)
+        {
+            _dependencyResolver = dependencyResolver;
+        }
+
+        protected override void Execute(ContextWrapper context)
+        {
+            _dependencyResolver.Init(typeof(DataLockEventsProcessor), context);
+
+            var processor = _dependencyResolver.GetInstance<DataLockEventsProcessor>();
+
+            processor.Process();
+        }
+    }
+}
---
+++ src/collections/SFA.DAS.Provider.Events.DataLock/Infrastructure/DependencyResolution/TaskDependencyResolver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SFA.DAS.Payments.DCFS.Context;
+using SFA.DAS.Payments.DCFS.StructureMap.Infrastructure.DependencyResolution;
+using StructureMap;
+
+namespace SFA.DAS.Provider.Events.DataLock.Infrastructure.DependencyResolution
+{
+    public class TaskDependencyResolver : StructureMapDependencyResolver
+    {
+        protected override Registry CreateRegistry(Type taskType, ContextWrapper contextWrapper)
+        {
+            return new DataLockEventsRegistry(taskType);
+        }
+    }
+}
