82623b4060788927bf89d42ce35eaf30cfbac129
Costin Grigorescu <costin.grigorescu@civil.lmco.com>
2017-02-10 08:55:31 +0000

    Update DataLock and Submissions controllers to require authorization roles.

--- src/api/SFA.DAS.Provider.Events.Api/Controllers/DataLockController.cs
+++ src/api/SFA.DAS.Provider.Events.Api/Controllers/DataLockController.cs
@@ -11,7 +11,7 @@ using SFA.DAS.Provider.Events.Domain.Mapping;
 namespace SFA.DAS.Provider.Events.Api.Controllers
 {
     [RoutePrefix("api/datalock")]
-    //[Authorize(Roles = "ReadDataLock")]
+    [Authorize(Roles = "ReadDataLock")]
     public class DataLockController : ApiController
     {
         private const int PageSize = 1000;
