866ec005a3dccde7e0a8de9ac77c13f3504a230e
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-07-25 14:14:27 +0100

    Add query to get current collection period
    Added new query to get the current collection period from the reference
    data database

---
+++ src/collections/SFA.DAS.Provider.Events.DataLock/Application/GetCurrentCollectionPeriod/GetCurrentCollectionPeriodResposne.cs
@@ -0,0 +1,9 @@
+﻿using SFA.DAS.Provider.Events.DataLock.Domain;
+
+namespace SFA.DAS.Provider.Events.DataLock.Application.GetCurrentCollectionPeriod
+{
+    public class GetCurrentCollectionPeriodResposne
+    {
+        public CollectionPeriod CollectionPeriod { get; set; }
+    }
+}
