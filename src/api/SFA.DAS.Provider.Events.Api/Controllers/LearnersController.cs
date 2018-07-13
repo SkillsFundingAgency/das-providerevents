197e0824697a8d50947e9aa8070ee80e58c88200
David Gouge <davegouge@gmail.com>
2018-07-13 10:47:02 +0100

    Moved new query to it's own endpoint / controller

---
+++ src/api/SFA.DAS.Provider.Events.Api/Controllers/LearnersController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using MediatR;
+using NLog;
+using SFA.DAS.Provider.Events.Application.Submissions.GetLatestLearnerEventByStandardQuery;
+
+namespace SFA.DAS.Provider.Events.Api.Controllers
+{
+    public class LearnersController : ApiController
+    {
+        private IMediator _mediator;
+        private ILogger _logger;
+
+        public LearnersController(IMediator mediator, ILogger logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+
+        [Route("api/v2/learners", Name = "GetLatestLearnerEventByStandard")]
+        [HttpGet]
+        public async Task<IHttpActionResult> GetLatestLearnerEventByStandard(long uln, long sinceEventId = 0)
+        {
+            try
+            {
+                _logger.Debug($"Processing GetLatestLearnerEventByStandard, uln={uln}, sinceEventId={sinceEventId}");
+
+                var queryResponse = await _mediator.SendAsync(new GetLatestLearnerEventByStandardQueryRequest
+                    {
+                        SinceEventId = sinceEventId,
+                        Uln = uln
+                    })
+                    .ConfigureAwait(false);
+
+                if (queryResponse.IsValid) return Ok(queryResponse.Result);
+
+                _logger.Info($"Bad request received to GetLatestLearnerEventByStandard - {queryResponse.Exception.Message}");
+                return BadRequest(queryResponse.Exception.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, $"Unexpected error processing GetLatestLearnerEventByStandard - {ex.Message}");
+                return InternalServerError();
+            }
+        }
+    }
+}
--- src/api/SFA.DAS.Provider.Events.Api/Controllers/SubmissionsController.cs
+++ src/api/SFA.DAS.Provider.Events.Api/Controllers/SubmissionsController.cs
@@ -4,7 +4,6 @@ using System.Web.Http;
 using MediatR;
 using NLog;
 using SFA.DAS.Provider.Events.Api.Plumbing.WebApi;
-using SFA.DAS.Provider.Events.Application.Submissions.GetLatestLearnerEventByStandardQuery;
 using SFA.DAS.Provider.Events.Application.Submissions.GetSubmissionEventsQuery;
 using SFA.DAS.Provider.Events.Application.Validation;
 
@@ -61,31 +60,6 @@ namespace SFA.DAS.Provider.Events.Api.Controllers
             }
         }
 
-        [Route("api/v2/submissions", Name = "GetLatestLearnerEventByStandard")]
-        [HttpGet]
-        public async Task<IHttpActionResult> GetLatestLearnerEventByStandard(long uln, long sinceEventId = 0)
-        {
-            try
-            {
-                _logger.Debug($"Processing GetLatestLearnerEventByStandard, uln={uln}, sinceEventId={sinceEventId}");
-
-                var queryResponse = await _mediator.SendAsync(new GetLatestLearnerEventByStandardQueryRequest
-                    {
-                        SinceEventId = sinceEventId,
-                        Uln = uln
-                    })
-                    .ConfigureAwait(false);
-
-                if (queryResponse.IsValid) return Ok(queryResponse.Result);
-
-                _logger.Info($"Bad request received to GetLatestLearnerEventByStandard - {queryResponse.Exception.Message}");
-                return BadRequest(queryResponse.Exception.Message);
-            }
-            catch (Exception ex)
-            {
-                _logger.Error(ex, $"Unexpected error processing GetLatestLearnerEventByStandard - {ex.Message}");
-                return InternalServerError();
-            }
-        }
+        
     }
 }
