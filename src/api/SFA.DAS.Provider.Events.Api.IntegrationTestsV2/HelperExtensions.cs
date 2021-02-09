80e2247a88561eaf4676b7cd8b02e593505f2894
Tom <Tom>
2021-02-09 10:00:51 +0000

    HelperExtensions for calendar month comparison

---
+++ src/api/SFA.DAS.Provider.Events.Api.IntegrationTestsV2/HelperExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace SFA.DAS.Provider.Events.Api.IntegrationTestsV2
+{
+    public static class HelperExtensions
+    {
+        public static int ToCalendarMonth(this byte collectionPeriod)
+        {
+            if (collectionPeriod < 6)
+                return collectionPeriod + 7;
+            return collectionPeriod - 5;
+        }
+    }
+}
