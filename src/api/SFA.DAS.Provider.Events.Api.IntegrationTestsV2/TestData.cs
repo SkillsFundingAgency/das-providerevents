c72cc4d193b897cf0d42120beef8551a2a8ab677
Akshay Chugh <akshay.chugh30@outlook.com>
2021-02-11 19:22:07 +0000

    PV2-2302 New property to support existing Payment records in DB

--- src/api/SFA.DAS.Provider.Events.Api.IntegrationTestsV2/TestData.cs
+++ src/api/SFA.DAS.Provider.Events.Api.IntegrationTestsV2/TestData.cs
@@ -5,6 +5,13 @@ namespace SFA.DAS.Provider.Events.Api.IntegrationTestsV2
 {
     class TestData
     {
+        /// <summary>
+        /// List of test payments inserted into the [Payments2].[Payment] table during this test run.
+        /// </summary>
         public static List<ItPayment> Payments { get; set; }
+        /// <summary>
+        /// List of all payments in the [Payments2].[Payment] table.
+        /// </summary>
+        public static List<ItPayment> AllPayments { get; set; }
     }
 }
