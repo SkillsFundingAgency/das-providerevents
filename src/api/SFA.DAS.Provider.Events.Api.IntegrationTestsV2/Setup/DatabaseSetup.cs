0f266b46043ab351eb426cb4f19e8c17480da383
Akshay Chugh <akshay.chugh30@outlook.com>
2021-02-12 12:01:42 +0000

    PV2-2202 Reducing test data 60000 -> 20000

--- src/api/SFA.DAS.Provider.Events.Api.IntegrationTestsV2/Setup/DatabaseSetup.cs
+++ src/api/SFA.DAS.Provider.Events.Api.IntegrationTestsV2/Setup/DatabaseSetup.cs
@@ -32,7 +32,7 @@ namespace SFA.DAS.Provider.Events.Api.IntegrationTestsV2.Setup
 
             var payments = new IEnumerable<ItPayment>[8];
             var fixture = new Fixture().Customize(new IntegrationTestCustomisation());
-            Parallel.For(0, 8, i => payments[i] = fixture.CreateMany<ItPayment>(7500));
+            Parallel.For(0, 8, i => payments[i] = fixture.CreateMany<ItPayment>(2500));
 
             TestData.Payments = payments.SelectMany(p => p).ToList();
             
