f8d1644d7e498fa3a9929cb1f952c63e43bd164b
Hardik Desai <hardik.desai@digital.education.gov.uk>
2021-02-24 15:18:45 +0000

    calculate AcademicYear and Period from PeriodId

--- src/api/SFA.DAS.Provider.Events.Application/Period/GetPeriodQuery/GetPeriodQueryRequest.cs
+++ src/api/SFA.DAS.Provider.Events.Application/Period/GetPeriodQuery/GetPeriodQueryRequest.cs
@@ -4,6 +4,10 @@ namespace SFA.DAS.Provider.Events.Application.Period.GetPeriodQuery
 {
     public class GetPeriodQueryRequest : IAsyncRequest<GetPeriodQueryResponse>
     {
-        public string PeriodId { get; set; }
+        public string PeriodId  { get; set; }
+
+        public short AcademicYear => short.Parse(PeriodId.Substring(0, 4));
+        
+        public byte Period => byte.Parse(PeriodId.Substring(6));
     }
 }
