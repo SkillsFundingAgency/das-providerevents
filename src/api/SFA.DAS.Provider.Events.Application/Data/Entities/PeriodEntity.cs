29540eb24258dc92e6df0966b845cf68619a799b
Hardik Desai <hardik.desai@digital.education.gov.uk>
2021-02-24 15:19:19 +0000

    use mapper to convert from PeriodEntity to CollectionPeriod

--- src/api/SFA.DAS.Provider.Events.Application/Data/Entities/PeriodEntity.cs
+++ src/api/SFA.DAS.Provider.Events.Application/Data/Entities/PeriodEntity.cs
@@ -7,10 +7,7 @@ namespace SFA.DAS.Provider.Events.Application.Data.Entities
         public int Id { get; set; }
         public short AcademicYear { get; set; }
         public byte Period { get; set; }
-        public int CalendarMonth { get; set; }
-        public int CalendarYear { get; set; }
-        public DateTime? AccountDataValidAt { get; set; }
-        public DateTime? CommitmentDataValidAt { get; set; }
-        public DateTime CompletionDateTime { get; set; }
+        public DateTime? ReferenceDataValidationDate { get; set; }
+        public DateTime CompletionDate { get; set; }
     }
 }
