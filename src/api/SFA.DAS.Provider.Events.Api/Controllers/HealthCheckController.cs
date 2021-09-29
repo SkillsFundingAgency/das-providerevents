61293ac91dae6382c57b8e90aa315c74a0580f68
EVANS <Christian.EVANS@EDUCATION.GOV.UK>
2021-09-29 16:24:29 +0100

    Changes to health check to make sure logging works correctly

--- src/api/SFA.DAS.Provider.Events.Api/Controllers/HealthCheckController.cs
+++ src/api/SFA.DAS.Provider.Events.Api/Controllers/HealthCheckController.cs
@@ -1,13 +1,19 @@
 ﻿using System.Web.Http;
 using SFA.DAS.Provider.Events.Api.Types;
+using NLog;
 
 namespace SFA.DAS.Provider.Events.Api.Controllers
 {
     [RoutePrefix("api/healthcheck")]
+    
     public class HealthCheckController : ApiController
     {
+        private readonly ILogger _logger;
+
         public IHttpActionResult Get()
         {
+            _logger.Error("Hello World");
+
             return Ok(new HealthStatus());
         }
     }
