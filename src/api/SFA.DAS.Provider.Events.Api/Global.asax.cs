0093e5a09f79df29053a9c1de8f72b843669ba27
Hardik Desai <hardik.desai@education.gov.uk>
2022-07-28 13:32:23 +0100

    fix for tls 1.1 deprecation

--- src/api/SFA.DAS.Provider.Events.Api/Global.asax.cs
+++ src/api/SFA.DAS.Provider.Events.Api/Global.asax.cs
@@ -5,6 +5,7 @@ using SFA.DAS.Provider.Events.Infrastructure.Logging;
 using Microsoft.Azure;
 using Microsoft.ApplicationInsights.Extensibility;
 using System.Configuration;
+using System.Net;
 
 namespace SFA.DAS.Provider.Events.Api
 {
@@ -12,6 +13,8 @@ namespace SFA.DAS.Provider.Events.Api
     {
         protected void Application_Start()
         {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Ssl3; // only allow TLSV1.2 and SSL3
+
             LoggingConfig.ConfigureLogging();
 
             TelemetryConfiguration.Active.InstrumentationKey = ConfigurationManager.AppSettings["APPINSIGHTS_INSTRUMENTATIONKEY"];
